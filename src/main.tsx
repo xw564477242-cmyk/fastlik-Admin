d1b447fe99e9152253760dcc0ba8bd2639d55b2d
xw564477242-cmyk <xw564477242@gmail.com>
2026-07-19 22:34:04 +0800

    style: load subsystem application styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import Root from './Root'
 import './styles.css'
 import './interactive.css'
+import './subsystem-apps.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><Root /></React.StrictMode>,
