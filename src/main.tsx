df2c93819d71ca548f6af8a33cb92fc6023acd3e
xw564477242-cmyk <xw564477242@gmail.com>
2026-07-20 21:54:14 +0800

    admin: secure src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './subsystem-apps.css'
 import './treasury-operations.css'
 import './tenant-operations.css'
 import './white-label-delivery.css'
+import './login.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><Root /></React.StrictMode>,
