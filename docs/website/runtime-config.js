8b7812911bce4380597f83f94dfa093099465376
xw564477242-cmyk <xw564477242@gmail.com>
2026-07-23 18:22:41 +0800

    deploy(website): add runtime-config.js to active Pages source

---
+++ docs/website/runtime-config.js
@@ -0,0 +1,6 @@
+window.__FASTLINK_RUNTIME__ = Object.freeze({
+  environment: 'PRODUCTION',
+  buildSha: '368067c6acc8bc733fa2a9d5f0a8493cd358eaa9',
+  apiUrl: 'https://exquisite-surprise-production-309d.up.railway.app/api',
+  apiUrlStatus: 'DEPLOYED'
+});
